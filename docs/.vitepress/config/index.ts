f898861bba3ec852a9ac1da36fbc30f12f3426a2
rex2630 <rex2630@gmail.com>
2026-04-03 18:53:21 +0200

    Add VitePress configuration for documentation

---
+++ docs/.vitepress/config/index.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitepress";
+import { en } from "./en";
+import { cs } from "./cs";
+
+export default defineConfig({
+  base: "/docs/",
+  locales: {
+    root: en,
+    cs: cs,
+  },
+});
