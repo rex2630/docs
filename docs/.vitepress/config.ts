844b64553b5feb6db8a3e66d46ee6e4891ca88b7
rex2630 <rex2630@gmail.com>
2026-04-03 17:33:19 +0200

    Add edit link configuration to VitePress docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,14 +3,19 @@ export default {
   description: "Documentation for P4G",
 
   themeConfig: {
+    editLink: {
+      pattern: "https://github.com/rex2630/docs/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+
     nav: [
       { text: "Home", link: "/" },
       {
         text: "Linux",
         items: [
-          { text: 'Quick Start', link: '/linux' },
-          { text: 'Test Category 2', link: '/linux/test' }
-        ]
+          { text: "Quick Start", link: "/linux" },
+          { text: "Test Category 2", link: "/linux/test" },
+        ],
       },
     ],
   },
