62135e7c1e942d88e07e31985064f6e6162552f7
rex2630 <rex2630@gmail.com>
2026-04-03 18:52:50 +0200

    Add Czech language configuration for P4G Docs

---
+++ docs/.vitepress/config/cs.ts
@@ -0,0 +1,42 @@
+export const cs = {
+  label: "Čeština",
+  lang: "cs",
+  link: "/cs/",
+  title: "P4G Docs",
+  description: "Dokumentace pro P4G",
+  themeConfig: {
+    editLink: {
+      pattern: "https://github.com/rex2630/docs/edit/main/docs/:path",
+      text: "Upravit tuto stránku na GitHubu",
+    },
+    nav: [
+      { text: "Domů", link: "/cs/" },
+      {
+        text: "Linux",
+        items: [
+          { text: "Rychlý start", link: "/cs/linux/" },
+          { text: "Alternativy k Windows aplikacím", link: "/cs/linux/alternatives" },
+        ],
+      },
+    ],
+    sidebar: {
+      "/cs/linux/": [
+        {
+          text: "Linux",
+          items: [
+            { text: "Rychlý start", link: "/cs/linux/" },
+            { text: "Alternativy k Windows aplikacím", link: "/cs/linux/alternatives" },
+          ],
+        },
+        {
+          text: "Průvodci instalací",
+          items: [
+            { text: "Linux Mint", link: "/cs/linux/install-mint" },
+            { text: "Ubuntu", link: "/cs/linux/install-ubuntu" },
+            { text: "Fedora", link: "/cs/linux/install-fedora" },
+          ],
+        },
+      ],
+    },
+  },
+};
