13f2d4ae97692dab634e8852f04932db015be168
rex2630 <rex2630@gmail.com>
2026-04-04 15:14:24 +0200

    Update en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -1,46 +1,47 @@
+// .vitepress/config/en.ts
 export const en = {
-  label: "English",
-  lang: "en",
-  title: "P4G Docs",
-  description: "Documentation for P4G",
+  label: 'English',
+  lang: 'en',
+  title: 'P4G Docs',
+  description: 'Documentation for P4G',
   themeConfig: {
     editLink: {
-      pattern: "https://github.com/rex2630/docs/edit/main/docs/:path",
-      text: "Edit this page on GitHub",
+      pattern: 'https://github.com/rex2630/docs/edit/main/docs/:path',
+      text: 'Edit this page on GitHub',
     },
     nav: [
-      { text: "Home", link: "/" },
+      { text: 'Home', link: '/en/' },
       {
-        text: "Linux",
+        text: 'Linux',
         items: [
-          { text: "Quick Start Guide", link: "/linux/" },
-          { text: "Windows App Alternatives", link: "/linux/alternatives" },
+          { text: 'Quick Start Guide', link: '/en/linux/' },
+          { text: 'Windows App Alternatives', link: '/en/linux/alternatives' },
         ],
       },
     ],
     sidebar: {
-      "/linux/": [
+      '/en/linux/': [
         {
-          text: "Linux",
+          text: 'Linux',
           items: [
-            { text: "Quick Start", link: "/linux/" },
+            { text: 'Quick Start', link: '/en/linux/' },
           ],
         },
         {
-          text: "Installation Guides",
+          text: 'Installation Guides',
           items: [
-            { text: "Linux Mint", link: "/linux/install-mint" },
-            { text: "Ubuntu", link: "/linux/install-ubuntu" },
-            { text: "Fedora", link: "/linux/install-fedora" },
+            { text: 'Linux Mint', link: '/en/linux/install-mint' },
+            { text: 'Ubuntu', link: '/en/linux/install-ubuntu' },
+            { text: 'Fedora', link: '/en/linux/install-fedora' },
           ],
         },
         {
-          text: "Software",
+          text: 'Software',
           items: [
-            { text: "Windows App Alternatives", link: "/linux/alternatives" },
+            { text: 'Windows App Alternatives', link: '/en/linux/alternatives' },
           ],
         },
       ],
     },
   },
-};
+}
