bb70bfdefecea7c869cfe8ac9ebff58ac8f5fbfa
rex2630 <rex2630@gmail.com>
2026-08-02 14:01:37 +0200

    chore(vitepress): use explicit .ts imports in config

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from "vitepress";
-import { en } from "./en";
-import { cs } from "./cs";
-import { pl } from "./pl";
+import { en } from "./en.ts";
+import { cs } from "./cs.ts";
+import { pl } from "./pl.ts";
 
 export default defineConfig({
   base: "/docs/",
-
   lastUpdated: true,
-
   locales: {
-    en: en,
-    cs: cs,
-    pl: pl,
+    en,
+    cs,
+    pl,
   },
   transformHead({ pageData }) {
     if (pageData.relativePath === "index.md") {
